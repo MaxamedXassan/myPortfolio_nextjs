6af2db5eb7fc36963eee54484d6771460c0bc4c7
Moha <hassanhuriye05@gmail.com>
2024-06-14 20:19:36 +0300

    navbar

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -1,42 +1,85 @@
-import React from 'react'
+"use client"
+import React, { useState } from 'react'
 
 const Navbar = () => {
-  return (
-   
 
-<nav className="bg-white dark:bg-gray-900 fixed w-full z-20 top-0 start-0 border-b border-gray-200 dark:border-gray-600">
-  <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-  <a href="https://flowbite.com/" className="flex items-center space-x-3 rtl:space-x-reverse">
-      <img src="https://flowbite.com/docs/images/logo.svg" className="h-8" alt="Flowbite Logo"/>
-      <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Flowbite</span>
-  </a>
-  <div className="flex md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
-      <button type="button" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Get started</button>
-      <button data-collapse-toggle="navbar-sticky" type="button" className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-sticky" aria-expanded="false">
-        <span className="sr-only">Open main menu</span>
-        <svg className="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
-            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 1h15M1 7h15M1 13h15"/>
-        </svg>
-    </button>
-  </div>
-  <div className="items-center justify-between hidden w-full md:flex md:w-auto md:order-1" id="navbar-sticky">
-    <ul className="flex flex-col p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
-      <li>
-        <a href="#" className="block py-2 px-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 md:dark:text-blue-500" aria-current="page">Home</a>
-      </li>
-      <li>
-        <a href="#" className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">About</a>
-      </li>
-      <li>
-        <a href="#" className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Services</a>
-      </li>
-      <li>
-        <a href="#" className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Contact</a>
-      </li>
-    </ul>
-  </div>
-  </div>
-</nav>
+  const [click, setClick] = useState(false)
+
+  const toggleNavbar = () => {
+    setClick(!click)
+  }
+  return (
+   <>
+   <nav className='bg-black'>
+    <div className='max-w-7xl mx-auto px-4 sm:px-6 lg:px-8'>
+      <div className='flex items-center justify-between h-16'>
+        <div className='flex items-center'>
+          <div className='flex-shrink-0'>
+            <a href="/" className='text-white'>
+            Logo</a>
+          </div>
+        </div>
+        <div className='hidden md:block'>
+          <div className='ml-4 flex items-center space-x-4'>
+            <a href="/" className='text-white hover:bg-white hover:text-black rounded-lg p-2'>Home</a>
+            <a href="/" className='text-white hover:bg-white hover:text-black rounded-lg p-2'>About</a>
+            <a href="/" className='text-white hover:bg-white hover:text-black rounded-lg p-2'>Projects</a>
+            <a href="/" className='text-white hover:bg-white hover:text-black rounded-lg p-2'>Contact</a>
+          </div>
+        </div>
+          <div className='md:hidden flex items-center'>
+            <button
+            className='inline-flex items-center justify-center p-2 rounded-md text-white md:text-white hover:text-white
+            focus:ring-inset focus:ring-white' onClick={toggleNavbar}>
+              {click ? (
+                <svg 
+                className='h-6 w-6'
+                xmlns="http://www.w3.org/200/svg"
+                fill="none"
+                viewBox='0 0 24 24'
+                stroke='currentColor'
+                >
+
+                <path
+                strokeLinecap='round'
+                strokeLinejoin='round'
+                strokeWidth={2}
+                d='M6 18L18 6M6 6l12 12'
+                />
+                </svg>
+              ): (
+                <svg 
+                className='h-6 w-6'
+                xmlns="http://www.w3.org/200/svg"
+                fill="none"
+                viewBox='0 0 24 24'
+                stroke='currentColor'
+                >
+
+                <path
+                strokeLinecap='round'
+                strokeLinejoin='round'
+                strokeWidth={2}
+                d='M4 6h16M4 12h16m-7  6h7'
+                />
+                </svg>
+              )}
+            </button>
+          </div>
+      </div>
+    </div>
+    {click && (
+      <div className='md:hidden'>
+        <div className='px-2 pt-2 pb-3 space-y-1 sm:px-3 justify-center'>
+            <a href="/" className='text-white block hover:bg-white hover:text-black rounded-lg p-2'>Home</a>
+            <a href="/" className='text-white block hover:bg-white hover:text-black rounded-lg p-2'>About</a>
+            <a href="/" className='text-white block hover:bg-white hover:text-black rounded-lg p-2'>Projects</a>
+            <a href="/" className='text-white block hover:bg-white hover:text-black rounded-lg p-2'>Contact</a>
+        </div>
+      </div>
+    )}
+   </nav>
+   </>
 
   )
 }
